3a6cddd8c53bc798fd46deec4b7e8dcabbe90387
Jon Arribas <jon.arribas@redbility.com>
2012-08-05 08:02:41 +0200

    Innitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0fd2480e3d292bfeb86057db6a859ef581bf652b03d94cdbae84395c88fe3466c9082754a586d70547d185ca2a4c7351e60cb6ca9f3d93c815ab332703ef5f49'
